cffcddc665ff720eb982d7b3da80c074e7add0cb
akane9506 <akane19950611@gmail.com>
2025-10-16 22:17:45 -0700

    repl updated

--- repl/repl.go
+++ repl/repl.go
@@ -6,7 +6,7 @@ import (
 	"io"
 
 	"github.com/akane9506/fake-ts/lexer"
-	"github.com/akane9506/fake-ts/token"
+	"github.com/akane9506/fake-ts/parser"
 )
 
 const PROMPT = ">>"
@@ -22,8 +22,21 @@ func Start(in io.Reader, out io.Writer) {
 		}
 		line := scanner.Text()
 		l := lexer.New(line)
-		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-			fmt.Printf("%+v\n", tok)
+		p := parser.New(l)
+
+		program := p.ParseProgram()
+		if len(p.Errors()) != 0 {
+			printParserError(out, p.Errors())
+			continue
 		}
+
+		io.WriteString(out, program.String())
+		io.WriteString(out, "\n")
+	}
+}
+
+func printParserError(out io.Writer, errors []string) {
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
 	}
 }
