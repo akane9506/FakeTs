6aa9d74a43bd4dce3499449499703ba69206a2c9
akane9506 <akane19950611@gmail.com>
2025-11-16 21:36:25 -0800

    closure test case updated

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -221,6 +221,18 @@ func TestFunctionApplication(t *testing.T) {
 	}
 }
 
+func TestClosures(t *testing.T) {
+	input := `
+let newAdder = fn(x) {
+	fn(y) { x + y; };
+};
+
+let addTwo = newAdder(2);
+addTwo(2)
+`
+	testIntegerObject(t, testEval(input), 4)
+}
+
 // helper functions
 func testEval(input string) object.Object {
 	l := lexer.New(input)
